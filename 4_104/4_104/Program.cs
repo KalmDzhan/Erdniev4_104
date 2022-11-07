60f925b32a188cd5b6baf4220017d6073832eacc
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2022-11-07 11:27:12 +0300

    Add files via upload

---
+++ 4_104/4_104/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите номер дня недели: ");
+int week = Convert.ToInt32(Console.ReadLine());
+
+switch (week)
+{
+    case 1:
+        Console.WriteLine(day.Понедельник);
+        break;
+    case 2:
+        Console.WriteLine(day.Вторник);
+        break;
+    case 3:
+        Console.WriteLine(day.Среда);
+        break;
+    case 4:
+        Console.WriteLine(day.Четверг);
+        break;
+    case 5:
+        Console.WriteLine(day.Пятница);
+        break;
+    case 6:
+        Console.WriteLine(day.Суббота);
+        break;
+    case 7:
+        Console.WriteLine(day.Вокресенье);
+        break;
+    default:
+        Console.WriteLine("Такого дня нет");
+        break;
+
+}
+
+enum day
+{
+    Понедельник,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятница,
+    Суббота,
+    Вокресенье
+}
